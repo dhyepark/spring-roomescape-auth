dba22aa9064d2abaeb80c87c85688ff938d9c277
dahye <dahye9011@naver.com>
2026-05-19 19:56:27 +0900

    feat: LoginMember 어노테이션 및 LoginMemberArgumentResolver 구현

---
+++ src/main/java/roomescape/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,48 @@
+package roomescape.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import roomescape.auth.exception.AuthenticationException;
+import roomescape.member.domain.Member;
+import roomescape.member.repository.MemberRepository;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberRepository memberRepository;
+
+    public LoginMemberArgumentResolver(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAnnotation = parameter.hasParameterAnnotation(LoginMember.class);
+        boolean isMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        return hasAnnotation && isMemberType;
+    }
+
+    @Nullable
+    @Override
+    public Object resolveArgument(@NonNull MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (request == null) {
+            throw new AuthenticationException();
+        }
+
+        Long memberId = (Long) request.getAttribute("memberId");
+        if (memberId == null) {
+            throw new AuthenticationException();
+        }
+
+        return memberRepository.findById(memberId)
+                .orElseThrow(AuthenticationException::new);
+    }
+}
