14ed0f8b300dcb5e3a2393ef1c01b5fb03d0009e
dahye <dahye9011@naver.com>
2026-05-19 19:39:14 +0900

    feat: LoginCheckInterceptor 구현

---
+++ src/main/java/roomescape/auth/LoginCheckInterceptor.java
@@ -0,0 +1,38 @@
+package roomescape.auth;
+
+import java.util.Arrays;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import roomescape.auth.exception.AuthenticationException;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    private final JwtProvider jwtProvider;
+
+    public LoginCheckInterceptor(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null) {
+            throw new AuthenticationException();
+        }
+
+        String token = Arrays.stream(cookies)
+                .filter(c -> c.getName().equals("token"))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(AuthenticationException::new);
+
+        jwtProvider.getId(token);
+        return true;
+    }
+}
+
