8a19a16f8cda526d21cfa6e1f11c30b21aafe51b
dahye <dahye9011@naver.com>
2026-05-21 15:57:09 +0900

    feat: storeId 추가

--- src/main/java/roomescape/theme/service/dto/ThemeSaveServiceDto.java
+++ src/main/java/roomescape/theme/service/dto/ThemeSaveServiceDto.java
@@ -1,3 +1,3 @@
 package roomescape.theme.service.dto;
 
-public record ThemeSaveServiceDto(String name, String description, String imageUrl) {}
+public record ThemeSaveServiceDto(String name, String description, String imageUrl, Long storeId) {}
